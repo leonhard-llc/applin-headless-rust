05ea194e15798ea3b5b8d9807d74e6cbfef7918a
Michael Leonhard <michael@leonhardllc.com>
2024-11-03 11:13:02 -0800

    Update scripts.

--- check-readme.sh
+++ check-readme.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")"
-echo PWD=$PWD
 set -e
 set -x
 cat Readme.md |perl -0777 -pe 's/(# Cargo Geiger Safety Report).+?```.+?```/$1/s' >Readme.md.pruned
 cargo readme --no-title --no-indent-headings >Readme.md.tmp
 diff Readme.md.pruned Readme.md.tmp || (
   set +x
-  echo "ERROR: Readme.md is stale" >&2
+  echo "ERROR: $(pwd)/Readme.md is stale" >&2
   exit 1
 )
 rm -f Readme.md.pruned Readme.md.tmp
